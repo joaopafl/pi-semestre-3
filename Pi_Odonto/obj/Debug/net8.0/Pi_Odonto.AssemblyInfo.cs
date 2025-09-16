309f10b5198e3f415b2852c9e7cb91cb32fe6fb8
Nicolly <nicolly.bueno@fatec.sp.gov.br>
2025-09-16 20:09:10 -0300

    configurando branch

--- Pi_Odonto/obj/Debug/net8.0/Pi_Odonto.AssemblyInfo.cs
+++ Pi_Odonto/obj/Debug/net8.0/Pi_Odonto.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pi_Odonto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39bc03c353f4a34e53b3bbb1f9f7db167e2369a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pi_Odonto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pi_Odonto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
