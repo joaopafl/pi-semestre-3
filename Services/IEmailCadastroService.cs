b90271bb7dbd72933b7c5c48d9d9cbd79a248ff1
Joao Paulo <joaopaf@gmail.com>
2025-10-31 20:47:36 -0300

    Limpando

--- Services/IEmailCadastroService.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace Pi_Odonto.Services
-{
-    public interface IEmailCadastroService
-    {
-        Task EnviarEmailBoasVindasAsync(string email, string nome);
-        Task EnviarEmailVerificacaoAsync(string email, string nome, string tokenVerificacao);
-    }
-}
